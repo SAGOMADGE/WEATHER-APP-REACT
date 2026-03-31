7b61954694da53366dc6517852516b14bf9ffaa2
SAGOMADGE <temokishmariya@gmail.com>
2026-03-31 20:03:14 +0300

    refactor: first refactor in validateCity.ts

--- src/utils/validateCity.ts
+++ src/utils/validateCity.ts
@@ -1,4 +1,6 @@
-function validateCity(value) {
+function validateCity(value: unknown): boolean {
+  if (typeof value !== "string") return false;
+
   const regex = /^[a-zA-Zа-яА-ЯёЁ\- ]+$/;
   return regex.test(value);
 }
